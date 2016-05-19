294b775459e533201b23364a5f235706d4abf9c8
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-05-19 15:16:13 +0100

    Add pseudocode for the main

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// $(function() {
+//   var
+//     apikey     = "9wur7sdh84azzazdt3ye54k4",
+//     connection = new Connection(Logger, apikey),
+//     parser     = new Parser(Validator)
+//   ;
+//    UI.setup(connection, parser);
+//    UI.loadArticles(UI.fetchingTrigger1, UI.tabOutput1, connection, parser);
+// });
